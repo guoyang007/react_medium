f83b0a1ee9f29879c64bd986213bef02446d6ed7
qixiuss <177811254@qq.com>
2016-08-16 17:53:35 +0800

    开发side-menu

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,22 +1,28 @@
 'use strict';
 
 import React from 'react';
+import SideMenu from 'components/side-menu/index.js';
 
 require('./index.less');
 
 class Header extends React.Component {
     render() {
-        return ( < div className = "com-header" >
-            < span className = "iconfont icon-menu" > < /span> < span className = "iconfont icon-logo" > < /span>  < span className = "login" > Login < /span> 
-            < /div>
+        return (
+        	<div className="com-header">
+				<SideMenu />
+
+            	<span className="iconfont icon-menu"></span> 
+
+            	<span className="iconfont icon-logo"></span>  
+            	<span className = "login">Login</span> 
+            </div>
         );
     }
 }
 
 Header.displayName = 'Header';
 
-// Uncomment properties you need
-// Header.propTypes = {};
-// Header.defaultProps = {};
+Header.propTypes = {};
+Header.defaultProps = {};
 
 export default Header;
