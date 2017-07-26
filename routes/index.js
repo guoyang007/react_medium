b445c4792cf625279ffe4e30e1373420b047600a
guo <1599485508@qq.com>
2017-07-26 23:34:26 +0800

    完善登录/注册--错误反馈

--- routes/index.js
+++ routes/index.js
@@ -1,148 +1,172 @@
-var express=require('express');
-var router=express.Router();
-var mongoose=require('mongoose');
-var articles=db.model('Articles');
-var detail=db.model('Detail');
-var comments=db.model('Comments');
-var users=db.model('Users');
-var bcrypt=require('bcrypt');
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var articles = db.model('Articles');
+var detail = db.model('Detail');
+var comments = db.model('Comments');
+var users = db.model('Users');
+var bcrypt = require('bcrypt');
 
 /*
 get home page
  */
-router.get('/',function(req,res,next){
-	res.render('index',{title:"medium-react"});
+router.get('/', function(req, res, next) {
+    res.render('index', { title: "medium-react" });
 
 });
-router.get('/articles',function(req,res,next){
-	articles.find({},function(err,results){
-		if(err){
-			console.log('error message',err);
-			return;
-		}
-		res.json(results);
-	})
+router.get('/articles', function(req, res, next) {
+    articles.find({}, function(err, results) {
+        if (err) {
+            console.log('error message', err);
+            return;
+        }
+        res.json(results);
+    })
 });
-router.get('/article/:id',function(req,res,next){
-	var id=req.params.id;
-	detail.findOne({id:id},function(err,results){
-		if(err){
-			console.log('error message',err);
-			return;
-		}
-		res.json(results)
+router.get('/article/:id', function(req, res, next) {
+    var id = req.params.id;
+    detail.findOne({ id: id }, function(err, results) {
+        if (err) {
+            console.log('error message', err);
+            return;
+        }
+        res.json(results)
 
-	})
+    })
 
 });
-router.get('/comments/:id',function(req,res){
-	var id=req.params.id;
+router.get('/comments/:id', function(req, res) {
+    var id = req.params.id;
 
-	comments.findOne({articleId:id},function(err,results){
-		if(err){
-			console.log('error message',err);
-			return;
-		}
-		res.json(results);
-	})
+    comments.findOne({ articleId: id }, function(err, results) {
+        if (err) {
+            console.log('error message', err);
+            return;
+        }
+        res.json(results);
+    })
 });
-router.post('/post_praise',function(req,res){
-	console.log(112233,req.body);
-	var data=req.body;
-	//可以看出数据结构很直观的会影响到数据的查找，这儿的update就稍微复杂了些
-	comments.update({'articleId':1,'comments.id':data.id},
-		{'comments.$.praise_count':data.praise_count,
-				'comments.$.praise':data.praise},
-		function(err,results){
-			if (err) {
-				console.log('error message',err);
-				return;
-			}
-			res.send({
-				praise_count:data.praise_count,
-				praise:data.praise
-			});	
-	})
+router.post('/post_praise', function(req, res) {
+    console.log(112233, req.body);
+    var data = req.body;
+    //可以看出数据结构很直观的会影响到数据的查找，这儿的update就稍微复杂了些
+    comments.update({ 'articleId': 1, 'comments.id': data.id }, {
+            'comments.$.praise_count': data.praise_count,
+            'comments.$.praise': data.praise
+        },
+        function(err, results) {
+            if (err) {
+                console.log('error message', err);
+                return;
+            }
+            res.send({
+                praise_count: data.praise_count,
+                praise: data.praise
+            });
+        })
 });
-router.post('/post_comment',function(req,res){
-	var data=req.body;
-	console.log(111,data);
-	// if (data.isReplyComment) {
-	// 	层级好深，不太会找，子评论部分后边再添加
-	// }
-	//给主comment增加一条记录
-	comments.update({'articleId':1},{$push:{'comments':{
-				'author' : {
-		           'id' : 242,
-		           'name' : 'new',
-		           'avatar' : 'http://www.qdaily.com/images/missing_face.png'
-		        },
-			    'praise_count' : 5,
-			    'praise' : true,
-			    'publish_time' : '2016-08-18 14:28:36 +0800',
-			    'content' : 'what the fuck.',
-			    'id' : 12
-			}}}, {upsert:true},function(err,results){
-				if (err) {
-					console.log(err)
-				}
-				console.log("succeed");
-				res.json(results);
-			})
+router.post('/post_comment', function(req, res) {
+    var data = req.body;
+    console.log(111, data);
+    // if (data.isReplyComment) {
+    //  层级好深，不太会找，子评论部分后边再添加
+    // }
+    //给主comment增加一条记录
+    comments.update({ 'articleId': 1 }, {
+        $push: {
+            'comments': {
+                'author': {
+                    'id': 242,
+                    'name': 'new',
+                    'avatar': 'http://www.qdaily.com/images/missing_face.png'
+                },
+                'praise_count': 5,
+                'praise': true,
+                'publish_time': '2016-08-18 14:28:36 +0800',
+                'content': 'what the fuck.',
+                'id': 12
+            }
+        }
+    }, { upsert: true }, function(err, results) {
+        if (err) {
+            console.log(err)
+        }
+        console.log("succeed");
+        res.json(results);
+    })
 })
 
 //signup
-router.post('/users/signup',function(req,res){
-	var data=req.body;
-	console.log("signup",data)
-	
-	users.find({name:data.name},function(err,result){
-		if (err) {
-			console.log(err)
-		}
-		if (result.length) {
-			console.log('该用户名已注册')
-			res.send({status:'error',data:'该用户名已注册'})
-		}else{
-			var user = new users(data)
-			user.save(function(err){
-				if (err) {
-					console.log(err);
-				}else{
-					console.log('save ok')
-				}
-			});
-			res.send({
-				status:'ok',
-				data:'已经注册成功'
-			})
-		}
-	})
-	
+router.post('/users/signup', function(req, res) {
+    var data = req.body;
+    console.log("signup", data)
+
+    users.find({ name: data.name }, function(err, result) {
+        if (err) {
+            console.log(err)
+        }
+        if (result.length) {
+            console.log('该用户名已注册')
+            res.send({ status: 'error', msg: '该用户名已注册' })
+        } else {
+            var user = new users(data)
+            user.save(function(err) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log('save ok')
+                }
+            });
+            res.send({
+                status: 'ok',
+                msg: '已经注册成功',
+                data: {
+                    name: data.name
+                }
+            })
+        }
+    })
+
 })
-router.post('/users/signIn',function(req,res){
-	var data=req.body;
-	users.findOne({name:data.name},function(err,result){
-		if (err) {
+router.post('/users/signIn', function(req, res) {
+    var data = req.body;
+    console.log(111,data)
+    users.findOne({ name: data.name }, function(err, result) {
+        if (err) {
             console.log(err);
+            return
         }
         if (!result) {
             console.log('登录失败，用户名不存在！');
-            return
+            return res.send({
+                    status:'error',
+                    msg:'login failed'
+            })
+
         }
-        bcrypt.compare(data.password,result.password,function(err,isMatch){
-        	 if (err) {
+        bcrypt.compare(data.password, result.password, function(err, isMatch) {
+            if (err) {
                 console.log(err);
+                return
             }
             if (isMatch) {
-            	console.log('登录成功！');
-            	return res.redirect('/');
-            }else{
-            	console.log('登录失败')
-            	return 
+                console.log('登录成功！');
+                res.send({
+                    status:'ok',
+                    msg:'login succuess',
+                    data:{
+                        name:data.name
+                    }
+                })
+            } else {
+                console.log('登录失败')
+                res.send({
+                    status:'error',
+                    msg:'login failed'
+                })
             }
         })
-	})
+    })
 })
 
-module.exports=router;
+module.exports = router;
--- src/components/login-form/index.js
+++ src/components/login-form/index.js
@@ -10,173 +10,221 @@ const FormItem = Form.Item;
 
 require('./index.less');
 
-class LoginForm extends React.Component{
-	constructor(props){
-		super(props);
-		this.state={
-			signIn:true,
-			confirmDirty:false,
-			name:'',
-			password:'',
-			repassword:''
-		}
-	}
-	handleSubmit = (e) => {
-	   e.preventDefault();
-	   this.props.form.validateFields((err, values) => {
-	     if (!err) {
-	       console.log('Received values of form: ', values);
-			fetch('/users/signIn',{
-				method:'POST',
-				headers:{
-					'Accept':'application/json',
-					'Content-Type': 'application/json'
-				},
-				body:JSON.stringify(values)
-			}).then(response=>{
-				if (response.status==200) {
-					this.closeLoginPopup(values.name);
-					console.log(123,response)
-				}
-			}).catch(err=>{
-				console.error(err);
-				this.closeLoginPopup();
-			})
-	     }
-	   });
-	}
+class LoginForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            signIn: true,
+            confirmDirty: false,
+            name: '',
+            password: '',
+            repassword: '',
+            loginSucceed:true,
+            registerSucceed:true
+        }
+    }
 
-	changeFormToRegister=()=>{
-		this.setState({
-			signIn:false
-		})
-	}
-	changeFormToLogin=()=>{
-		this.setState({
-			signIn:true
-		})
-	}
-	register=(e)=>{
-		e.preventDefault();
-	    this.props.form.validateFieldsAndScroll((err, values) => {
-	      if (!err) {
-	        console.log('Received values of form: ', values);
-	        fetch('/users/signup',{
-				method:'POST',
-				headers:{
-					'Accept':'application/json',
-					'Content-Type': 'application/json'
-				},
-				body:JSON.stringify(values)
-			}).then(response=>{
-				if (response.status==200) {
-					this.closeLoginPopup(values.name);
-					console.log(123,response.json())
-				}
-			}).catch(err=>{
-				console.error(err);
-				this.closeLoginPopup();
-			})
-	      }
-	    });
-	}
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+                fetch('/users/signIn', {
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(values)
+                }).then(response=>{
+                    return response.json();
+                }).then(obj => {
+                    console.log('obj',obj)
+                    if (obj.status == 'ok') {
+                        this.closeLoginPopup(obj.data.name);
+                        console.log(123, obj)
+                    }else{
+                        this.setState({
+                            loginSucceed:false
+                        })
+                        let that=this;
+                        setTimeout(function(){
+                            that.setState({
+                                loginSucceed:true
+                            })
+                        },2000)
+                    }
+                }).catch(error => {
+                    console.error(error);
+                    this.closeLoginPopup();
+                })
+            }
+        });
+    }
 
-	handleConfirmBlur = (e) => {
-	    const value = e.target.value;
-	    this.setState({ confirmDirty: this.state.confirmDirty || !!value });
-	  }
-	checkPassword = (rule, value, callback) => {
-	    const form = this.props.form;
-	    if (value && value !== form.getFieldValue('password')) {
-	      callback('Two passwords that you enter is inconsistent!');
-	    } else {
-	      callback();
-	    }
-	  }
-	// inputBlur(){
-	// 	if (this.state.password===this.state.repassword) {
-	// 		console.log('ok');
-	// 	}else{
-	// 		console.log(' 密码不匹配 ')
-	// 	}
-	// }
-	//提交后，触发
-	closeLoginPopup=(name)=>{
-		eventProxy.trigger('Login::Close',{name:name});
-	}
-	renderSignIn=()=>{
-		const { getFieldDecorator } = this.props.form;
-		return (
-			<Form onSubmit={this.handleSubmit} className="login-form">
-				<FormItem>
-		          {getFieldDecorator('name', {
-		            rules: [{ required: true, message: 'Please input your userName!' }],
-		          })(
-		            <Input prefix={<Icon type="user" style={{ fontSize: 13 }} />} placeholder="Username" />
-		          )}
-		        </FormItem>
-		        <FormItem>
-	              {getFieldDecorator('password', {
-	                rules: [{ required: true, message: 'Please input your Password!' }],
-	              })(
-	                <Input prefix={<Icon type="lock" style={{ fontSize: 13 }} />} type="password" placeholder="Password" />
-	              )}
-	            </FormItem>
-	            <FormItem>
-	              <Button type="primary" htmlType="submit" className="login-form-button">
-	                Log in
-	              </Button>
-	              Or <a href="#" onClick={this.changeFormToRegister}>register now!</a>
-	            </FormItem>
-			</Form>	
-		)
-	}
-	renderSignUp=()=>{
-		const { getFieldDecorator } = this.props.form;
-		return (
-			<Form onSubmit={this.register} className="login-form">
-				<FormItem>
-		          {getFieldDecorator('name', {
-		            rules: [{ required: true, message: 'Please input your userName!' }],
-		          })(
-		            <Input prefix={<Icon type="user" style={{ fontSize: 13 }} />} placeholder="Username" />
-		          )}
-		        </FormItem>
-    	        <FormItem>
-                  {getFieldDecorator('password', {
-                    rules: [{ required: true, message: 'Please input your Password!' }],
-                  })(
-                    <Input prefix={<Icon type="lock" style={{ fontSize: 13 }} />} type="password" placeholder="Password" />
-                  )}
+    changeFormToRegister = () => {
+        this.setState({
+            signIn: false
+        })
+    }
+    changeFormToLogin = () => {
+        this.setState({
+            signIn: true
+        })
+    }
+    register = (e) => {
+        e.preventDefault();
+        this.props.form.validateFieldsAndScroll((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+                fetch('/users/signup', {
+                        method: 'POST',
+                        headers: {
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify(values)
+                    }).then(response => {
+                        return response.json();
+                    })
+                    .then(obj => {
+                        if (obj.status == 'ok') {
+                            this.closeLoginPopup(obj.data.name)
+                        }else{
+                            this.setState({
+                                registerSucceed:false
+                            })
+                            let that=this;
+                            setTimeout(function(){
+                                that.setState({
+                                    registerSucceed:true
+                                })
+                            },2000)
+}
+                    })
+                    .catch(err => {
+                        console.error(err);
+                        this.closeLoginPopup();
+                    })
+            }
+        });
+    }
+
+    handleConfirmBlur = (e) => {
+        const value = e.target.value;
+        this.setState({ confirmDirty: this.state.confirmDirty || !!value });
+    }
+    checkPassword = (rule, value, callback) => {
+            const form = this.props.form;
+            if (value && value !== form.getFieldValue('password')) {
+                callback('Two passwords that you enter is inconsistent!');
+            } else {
+                callback();
+            }
+        }
+
+    closeLoginPopup = (name) => {
+        eventProxy.trigger('Login::Close', { name: name });
+    }
+    renderSignIn = () => {
+        const { getFieldDecorator } = this.props.form;
+        return (
+        <Form onSubmit = {this.handleSubmit} className = "login-form">
+             <FormItem> {
+                getFieldDecorator('name', {
+                    rules: [{ required: true, message: 'Please input your userName!' }],
+                })( < Input prefix = {<Icon type = "user" style = {{ fontSize: 13 }} />} placeholder="Username" />)}
+            </FormItem>
+            <FormItem> {
+                getFieldDecorator('password', {
+                    rules: [{ required: true, message: 'Please input your Password!' }],})
+                (<Input prefix = { <Icon type = "lock"
+                    style = {{ fontSize: 13 }}/>} type="password" placeholder="Password" />
+                )
+            } </FormItem> 
+            <FormItem>
+                <Button type = "primary" htmlType = "submit" className = "login-form-button">
+                    Log in
+                </Button>
+            Or <a href = "#" onClick = {this.changeFormToRegister }> register now! </a> 
+            </FormItem>
+            {this.renderErrorTip()}
+        </Form>	
+        )
+    }
+
+    renderSignUp = () => {
+        const { getFieldDecorator } = this.props.form;
+        return ( 
+            <Form onSubmit = { this.register } className = "login-form">
+                <FormItem> {
+                    getFieldDecorator('name', {
+                        rules: [{ required: true, message: 'Please input your userName!' }],
+                    })( <Input prefix = { <Icon type = "user"
+                            style = {{ fontSize: 13 }}/>} placeholder="Username" />
+                        )
+                    } 
+                </FormItem> 
+                <FormItem> {
+                        getFieldDecorator('password', {
+                            rules: [{ required: true, message: 'Please input your Password!' }],
+                        })( <Input prefix = { <Icon type = "lock"
+                                style = {{ fontSize: 13 } }/>} type="password" placeholder="Password" />
+                            )
+                        } 
                 </FormItem>
-    	        <FormItem>
-                  {getFieldDecorator('confirm', {
-                    rules: [{ required: true, message: 'Please confirm your Password!' },
-                    {validator:this.checkPassword}],
-                  })(
-                    <Input prefix={<Icon type="lock" style={{ fontSize: 13 }} />} type="password" placeholder="Password" onBlur={this.handleConfirmBlur}/>
-                  )}
-                </FormItem>
-                <FormItem>
-                  <Button type="primary" htmlType="submit" className="login-form-button">
-                    Sign up
-                  </Button>
-                   <a href="#" onClick={this.changeFormToLogin}>已有账号，直接登录</a>
-                </FormItem>
-			</Form>
-		)
-	}
-	render(){
-		return (
-			<div className="com-login-form">
-				<div className="favicon">
-	                <p className="icon">M</p>
-	                <p className="favtext">给你想要的</p>
-	            </div>
-	            {this.state.signIn?this.renderSignIn():this.renderSignUp()}
-	        </div>
-		)
-	}
+                <FormItem> {
+                    getFieldDecorator('confirm', {
+                        rules: [{ required: true, message: 'Please confirm your Password!' },
+                            { validator: this.checkPassword }
+                        ],
+                    })( <Input prefix = { <Icon type = "lock"
+                            style = {
+                                { fontSize: 13 } }
+                            />} type="password" placeholder="Password" onBlur={this.handleConfirmBlur}/>
+                        )
+                    }
+                 </FormItem>
+                 <FormItem>
+                    <Button type = "primary" htmlType = "submit" className = "login-form-button">
+                        Sign up
+                    </Button>
+                    <a href = "#" onClick = { this.changeFormToLogin }>
+                        已有账号， 直接登录
+                    </a> 
+                </FormItem> 
+                {this.renderErrorTip()}
+            </Form>
+        )
+    }
+    renderErrorTip=()=>{
+        if(!this.state.registerSucceed){
+            return (
+                <div className="errorTip">
+                    <span>该用户名已被占用，请重新注册！</span>
+                </div>
+            )
+        }
+        if (!this.state.loginSucceed) {
+            return (
+                <div className="errorTip">
+                    <span>用户名或密码错误!</span>
+                </div>
+            )
+        }
+        return null
+    }
+    render() {
+        return ( 
+            <div className = "com-login-form" >
+                <div className = "favicon" >
+                    <p className = "icon" > M </p>
+                    <p className = "favtext"> 给你想要的 </p>
+                </div>
+                {this.state.signIn ? this.renderSignIn() : this.renderSignUp() }
+            </div>
+        )
+    }
 }
 const WrappedNormalLoginForm = Form.create()(LoginForm);
 export default WrappedNormalLoginForm
